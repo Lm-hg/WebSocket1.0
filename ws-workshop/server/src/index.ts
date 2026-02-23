a0ccdb9cc9121f9e6b0d46bd40eeade09d0c9ab5
Lm-hg <you@example.com>
2026-02-23 10:43:07 +0100

    exercice 1 : mise en place du websocket

--- ws-workshop/server/src/index.ts
+++ ws-workshop/server/src/index.ts
@@ -1,1 +1,21 @@
+import {WebSocketServer}  from "ws";
+
+
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("message", (message) => {
+    const messageStr = message.toString();
+    console.log(`Received message: ${messageStr}`);
+    ws.send(`Echo: ${messageStr}`);
+  });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+wss.on("listening", () => {
+  console.log("WebSocket server is listening on port 8080");
+});
+
 console.log("Server ready"); 
